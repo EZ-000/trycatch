a746f72879dac536605cd78339da86c3abb8c75d
khryu <hyeokkr@gmail.com>
2023-01-19 15:50:49 +0900

    Github Test 연결

---
+++ src/main/java/com/ssafy/trycatch/GithubToken.java
@@ -0,0 +1,43 @@
+package com.ssafy.trycatch;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Component
+public class GithubToken {
+	@JsonProperty("access_token")
+	private String accessToken;
+	@JsonProperty("token_type")
+	private String tokenType;
+	@JsonProperty("scope")
+	private String scope;
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+	public String getAuthorizationValue() {
+		return this.tokenType + " " + this.accessToken;
+	}
+}
