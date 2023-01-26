a1e26632fec2905fd7733775ca48c2bbe2bf8f2c
khryu <hyeokkr@gmail.com>
2023-01-26 11:50:14 +0900

    fix: login scope 수정, not null 오류 수정

--- src/main/java/com/ssafy/trycatch/common/infra/config/security/SecurityConfig.java
+++ src/main/java/com/ssafy/trycatch/common/infra/config/security/SecurityConfig.java
@@ -1,18 +1,16 @@
 package com.ssafy.trycatch.common.infra.config.security;
 
-import com.ssafy.trycatch.common.infra.config.auth.OAuth2SuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.ssafy.trycatch.common.infra.config.auth.CustomOAuth2UserService;
+import com.ssafy.trycatch.common.infra.config.auth.OAuth2SuccessHandler;
 import com.ssafy.trycatch.common.infra.config.jwt.JwtAuthFilter;
 import com.ssafy.trycatch.common.infra.config.jwt.TokenService;
-import com.ssafy.trycatch.user.domain.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -31,21 +29,16 @@ public class SecurityConfig {
 			.and()
 			.authorizeRequests()
 			.antMatchers("/token/**").permitAll()
-			.anyRequest().authenticated()
+			.anyRequest()
+			// 인증이 모두 필요
+			.authenticated()
+			// 인증을 모두 허용
+			//.permitAll()
 			.and()
-			// .addFilterBefore(new JwtAuthFilter(tokenService), OAuth2LoginAuthenticationFilter.class)
-			.addFilterBefore(new JwtAuthFilter(tokenService), UsernamePasswordAuthenticationFilter.class)
 			.oauth2Login()
 			.successHandler(successHandler)
 			.userInfoEndpoint().userService(oAuth2UserService);
-
-		// test
-		// http.httpBasic().disable()
-		// 	.csrf().disable()
-		// 	.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		// 	.and()
-		// 	.authorizeRequests()
-		// 	.anyRequest().permitAll();
+		http.addFilterBefore(new JwtAuthFilter(tokenService), OAuth2LoginAuthenticationFilter.class);
 
 		return http.build();
 	}
