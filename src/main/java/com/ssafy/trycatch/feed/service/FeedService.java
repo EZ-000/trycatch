63ea787ca5f02af81ee697ef5a3fca6d2f0b5d05
EZ-000 <nongdam20@naver.com>
2023-02-07 22:42:46 +0900

    feat: 북마크한 피드 조회

--- src/main/java/com/ssafy/trycatch/feed/service/FeedService.java
+++ src/main/java/com/ssafy/trycatch/feed/service/FeedService.java
@@ -4,6 +4,10 @@ import com.ssafy.trycatch.common.domain.Company;
 import com.ssafy.trycatch.common.domain.CompanyRepository;
 import com.ssafy.trycatch.elasticsearch.domain.ESFeed;
 import com.ssafy.trycatch.elasticsearch.domain.repository.ESFeedRepository;
+import com.ssafy.trycatch.feed.domain.Feed;
+import com.ssafy.trycatch.feed.domain.FeedRepository;
+import com.ssafy.trycatch.feed.service.exception.ESFeedNotFoundException;
+import com.ssafy.trycatch.feed.service.exception.FeedNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,15 +17,16 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class FeedService {
-
+    private final FeedRepository feedRepository;
     private final ESFeedRepository esFeedRepository;
     private final CompanyRepository companyRepository;
 
     @Autowired
     public FeedService(
-            ESFeedRepository esFeedRepository,
+            FeedRepository feedRepository, ESFeedRepository esFeedRepository,
             CompanyRepository companyRepository
     ) {
+        this.feedRepository = feedRepository;
         this.esFeedRepository = esFeedRepository;
         this.companyRepository = companyRepository;
     }
@@ -43,5 +48,15 @@ public class FeedService {
         return this.companyRepository.findById(companyId)
                                      .orElse(Company.builder().icon("").build())
                                      .getIcon();
+    }
+
+    public Feed findById(Long feedId) {
+        return feedRepository.findById(feedId)
+                .orElseThrow(FeedNotFoundException::new);
+    }
+
+    public ESFeed findByESId(String feedId) {
+        return esFeedRepository.findById(feedId)
+                .orElseThrow(ESFeedNotFoundException::new);
     }
 }
