512cf22fb8d17e8ea87b9858f8ccbff284ef4c32
Bogyeong Kim <boggyhint@gmail.com>
2023-02-07 09:05:58 +0900

    impl: adoc

--- src/test/java/com/ssafy/trycatch/qna/controller/QuestionControllerTest.java
+++ src/test/java/com/ssafy/trycatch/qna/controller/QuestionControllerTest.java
@@ -1,25 +1,22 @@
 package com.ssafy.trycatch.qna.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
-@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @PropertySource("classpath:application-local.yml")
 class QuestionControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
@@ -28,53 +25,54 @@ class QuestionControllerTest {
     @Value("${apiPrefix}")
     private String apiVersion;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext,
-               RestDocumentationContextProvider documentationContextProvider) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                                      .apply(MockMvcRestDocumentation.documentationConfiguration(
-                                              documentationContextProvider))
-                                      .build();
-    }
-
     @Test
     void findAllQuestions() {
+
     }
 
     @Test
     void createQuestion() {
+
     }
 
     @Test
     void putQuestion() {
+
     }
 
     @Test
     void deleteQuestion() {
+
     }
 
     @Test
     void findQuestionById() {
+
     }
 
     @Test
     void createAnswers() {
+
     }
 
     @Test
     void putAnswer() {
+
     }
 
     @Test
     void search() {
+
     }
 
     @Test
     void acceptAnswer() {
+
     }
 
     @Test
     void suggestQuestions() {
+
     }
 
 //    @Test
