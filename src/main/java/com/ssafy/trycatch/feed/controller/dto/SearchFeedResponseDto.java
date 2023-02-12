5ce164e9fca0448c19474ef6048002dc78099af1
ez000 <nongdam20@naver.com>
2023-02-12 21:56:24 +0900

    chore: feed 조회 시 isBookmarked 적용

--- src/main/java/com/ssafy/trycatch/feed/controller/dto/SearchFeedResponseDto.java
+++ src/main/java/com/ssafy/trycatch/feed/controller/dto/SearchFeedResponseDto.java
@@ -1,8 +1,10 @@
 package com.ssafy.trycatch.feed.controller.dto;
 
+import com.ssafy.trycatch.common.service.BookmarkService;
 import com.ssafy.trycatch.elasticsearch.domain.ESFeed;
 import com.ssafy.trycatch.feed.service.FeedService;
 
+import com.ssafy.trycatch.user.domain.User;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,13 +16,20 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.ssafy.trycatch.common.domain.TargetType.FEED;
+
 @Slf4j
 @Data
 public class SearchFeedResponseDto {
 
-    public static SearchFeedResponseDto of(Page<ESFeed> esFeedPage, FeedService feedService) {
+    public static SearchFeedResponseDto of(
+			Page<ESFeed> esFeedPage,
+			FeedService feedService,
+			BookmarkService bookmarkService,
+			User requestUser
+	) {
         return new SearchFeedResponseDto(esFeedPage.stream()
-                                                   .map(entity -> Item.of(entity, feedService))
+                                                   .map(entity -> Item.of(entity, feedService, bookmarkService, requestUser))
                                                    .collect(Collectors.toList()));
     }
 
@@ -57,10 +66,16 @@ public class SearchFeedResponseDto {
 
 		private String thumbnailImage;
 
-		static Item of(ESFeed entity, FeedService feedService) {
+		static Item of(
+				ESFeed entity,
+				FeedService feedService,
+				BookmarkService bookmarkService,
+				User requestUser) {
 			log.info(entity.getId());
+			final Long id = feedService.findByESId(entity.getId()).getId();
+
 			return Item.builder()
- 				.id(feedService.findByESId(entity.getId()).getId())
+ 				.id(id)
 				.feedId(entity.getId())
 				.title(entity.getTitle())
 				.summary(entity.getSummary())
@@ -71,7 +86,8 @@ public class SearchFeedResponseDto {
 				.url(entity.getUrl())
 				.tags(entity.getTags())
 				.keywords(entity.getKeywords())
-				.isBookmarked(false) // FIXME
+				.isBookmarked(bookmarkService
+						.isBookmarkByUserAndTarget(requestUser.getId(), id, FEED))
 				.thumbnailImage(entity.getThumbnailUrl())
 				.build();
 		}
