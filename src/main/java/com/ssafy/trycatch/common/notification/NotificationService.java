46cac9ab67c56d88077de0a5b6c053b05cd49c26
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2023-02-12 15:00:40 +0900

    fix : idle check time 설정

--- src/main/java/com/ssafy/trycatch/common/notification/NotificationService.java
+++ src/main/java/com/ssafy/trycatch/common/notification/NotificationService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.ssafy.trycatch.common.domain.Notification;
@@ -126,6 +127,7 @@ public class NotificationService {
 			.data(message));
 	}
 
+	@Transactional
 	public void sendSaved(SseEmitter sseEmitter, Long userId) {
 		List<Notification> savedNotifylist = notificationRepository.findByUserIdOrderByIdAsc(userId);
 		try {
