e9e6528fce395382984d58c2faf53a094d74a010
hyeok00 <56781876+hyeok00@users.noreply.github.com>
2023-02-12 16:58:47 +0900

    fix : findTop10ByUserIdOrderByIdDesc Query 수정 (중복 feedId는 최신으로 select)

--- src/main/java/com/ssafy/trycatch/feed/domain/ReadRepository.java
+++ src/main/java/com/ssafy/trycatch/feed/domain/ReadRepository.java
@@ -3,7 +3,11 @@ package com.ssafy.trycatch.feed.domain;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReadRepository extends JpaRepository<Read, Long> {
+	//List<Read> findTop10ByUserIdOrderByIdDesc(Long id);
+
+	@Query("select r from Read r where r.user.id = ?1 and (r.user.id,r.readAt) in (select sr.user.id, max(sr.readAt) from Read sr group by sr.user.id, sr.feed.id) order by r.id DESC limit 10")
 	List<Read> findTop10ByUserIdOrderByIdDesc(Long id);
 	}
